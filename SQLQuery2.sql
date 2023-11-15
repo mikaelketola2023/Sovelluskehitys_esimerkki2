8756dd7ce6889d84ead202436b236eab94c18da3
juhay-git <juha.yli-hemminki@seamk.fi>
2023-11-15 15:53:33 +0200

    Uusi TabItem ja sinne DataGrid

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,5 +1,7 @@
 CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi TEXT, hinta INTEGER);
 
+CREATE TABLE asiakkaat (id INTEGER IDENTITY (1,1) PRIMARY KEY, nimi VARCHAR(50), puhelinnumero VARCHAR(50));
+
 INSERT INTO tuotteet (nimi, hinta) VALUES ('maito', 2);
 
 SELECT * FROM tuotteet;
